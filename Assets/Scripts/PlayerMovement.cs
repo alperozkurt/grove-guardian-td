3f7167f6d2d137ac36eccd9288be2a767224a939
Alper Özkurt <alper98ozx@gmail.com>
2025-12-04 18:24:03 +0300

    camera test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    [Header("Input Actions")]
-    [SerializeField] private InputAction movement;
-    [SerializeField] private InputAction look;
-    [SerializeField] private InputAction jump;
+    [Header("References")]
+    public CharacterController controller;
+    public Transform cameraTransform;
 
-    [Header("Movement Settings")]
-    [SerializeField] private float jumpPower = 1.5f;
-    [SerializeField] private float movemenetSpeed = 5f;
-    private float gravity = -9.81f;
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
 
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private float yVelocity;
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        jumpPressed = value.isPressed;
+    }
+
+    private void MovePlayer()
+    {
+        // Kameranın yönüne göre forward/right elde et
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+
+        // Y eksenini yok et (eğik kameralarda yere gömülmemek için)
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        // Input’u kamera yönüne göre dönüştür
+        Vector3 moveDir = (camForward * moveInput.y + camRight * moveInput.x);
+
+        // Yerçekimi
+        if (controller.isGrounded)
+        {
+            yVelocity = -1f;
+            if (jumpPressed)
+            {
+                yVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        else
+        {
+            yVelocity += gravity * Time.deltaTime;
+        }
+
+        // Son hareket
+        Vector3 velocity = moveDir * moveSpeed + Vector3.up * yVelocity;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
