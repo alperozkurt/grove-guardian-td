e50926c53901caa8d528ade83726e645f52774da
Alper Özkurt <alper98ozx@gmail.com>
2025-12-04 16:47:59 +0300

    fixing the initial commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,53 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 6f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 1.5f;
-    private CharacterController controller;
-    private Vector3 velocity;
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    [Header("Input Actions")]
+    [SerializeField] private InputAction movement;
+    [SerializeField] private InputAction look;
+    [SerializeField] private InputAction jump;
 
-    // Update is called once per frame
-    void Update()
-    {
-        MovePlayer();
-        ApplyGravity();
-        Jump();
-    }
+    [Header("Movement Settings")]
+    [SerializeField] private float jumpPower = 1.5f;
+    [SerializeField] private float movemenetSpeed = 5f;
+    private float gravity = -9.81f;
 
-    void MovePlayer()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector3 direction = transform.right * horizontal + transform.forward * vertical;
-        controller.Move(direction * movementSpeed * Time.deltaTime);
-    }
-
-    void ApplyGravity()
-    {
-        if(controller.isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-    }
-
-    void Jump()
-    {
-        if(controller.isGrounded && Input.GetButtonDown("Jump"))
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-    }
 }
